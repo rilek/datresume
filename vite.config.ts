e72a191a55eb53d1a9ef608d109261015e569e9c
Rafal Ileczko <rileczko@gmail.com>
2025-10-10 14:55:15 +0200

    Update build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import tsConfigPaths from 'vite-tsconfig-paths'
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
