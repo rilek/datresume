aead2dc852a80eee003b9e64d3d55b641209ff87
Rafal Ileczko <rileczko@gmail.com>
2025-10-13 01:17:38 +0200

    Introduce pnpm-workspace

--- apps/frontend/src/utils/supabase/client.ts
+++ apps/frontend/src/utils/supabase/client.ts
@@ -1,5 +1,6 @@
 import { Database } from "@/types/supabase";
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;export const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
