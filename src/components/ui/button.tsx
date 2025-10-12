07c71a5882b818841b184235c984b9b3e3d56add
Rafal Ileczko <rileczko@gmail.com>
2025-10-12 23:33:27 +0200

    Update shadcn components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,18 +5,18 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded cursor-pointer text-sm font-medium transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-sm hover:bg-primary/90",
+          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-xs hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
@@ -29,24 +29,23 @@ const buttonVariants = cva(
         "icon-lg": "h-9 w-9 [&_svg]:size-5",
         "icon-sm": "h-8 w-8 [&_svg]:size-4"
       },
-  },
-  defaultVariants: {
-  variant: "default",
-  size: "default",
-},
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
   }
 )
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
-  component?: React.ElementType;
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, component, ...props }, ref) => {
-    const Comp = asChild ? Slot : component ?? "button"
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button"
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx"
+import { DetailedHTMLProps, HTMLAttributes } from "react"
+
+export function H1(props: DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) {
+  return (
+    <h1 {...props} className={clsx("scroll-m-20 text-2xl font-extrabold tracking-tight text-balance", props.className)} />
+  )
+}
+
+export function H2(props: DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) {
+  return (
+    <h2 {...props} className={clsx("scroll-m-20 border-b pb-2 text-xl font-semibold tracking-tight first:mt-0", props.className)} />
+  )
+}
+
+export function H3(props: DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) {
+  return (
+    <h3 {...props} className={clsx("scroll-m-20 text-lg font-semibold tracking-tight", props.className)} />
+  )
+}
+
+export function H4(props: DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) {
+  return (
+    <h4 {...props} className={clsx("scroll-m-20 text-base font-semibold tracking-tight", props.className)} />
+  )
+}
+
+export function P(props: DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>) {
+  return (
+    <p {...props} className={clsx("leading-7 [&:not(:first-child)]:mt-6", props.className)} />
+  )
+}
+
